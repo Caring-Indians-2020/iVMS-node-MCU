98653c1d4ce92c9fda12d5a705d22dec4a8865dc
Rohit Gupta <guro@cypress.com>
2020-04-29 01:12:11 +0530

    Additional Libraries Added

---
+++ ArduinoSketches/Libraries/ConfigManager/src/ConfigManager.cpp
@@ -0,0 +1,368 @@
+#include "ConfigManager.h"
+
+const byte DNS_PORT = 53;
+
+void ConfigManager::setAPName(const char *name) {
+    this->apName = (char *)name;
+}
+
+void ConfigManager::setAPFilename(const char *filename) {
+    this->apFilename = (char *)filename;
+}
+
+void ConfigManager::setAPICallback(std::function<void(ESP8266WebServer*)> callback) {
+    this->apiCallback = callback;
+}
+
+void ConfigManager::loop() {
+    if (server) {
+        server->handleClient();
+    }
+}
+
+void ConfigManager::save() {
+    this->writeConfig();
+}
+
+JsonObject &ConfigManager::decodeJson(String jsonString)
+{
+    DynamicJsonBuffer jsonBuffer;
+
+    if (jsonString.length() == 0) {
+        return jsonBuffer.createObject();
+    }
+
+    JsonObject& obj = jsonBuffer.parseObject(jsonString);
+
+    if (!obj.success()) {
+        return jsonBuffer.createObject();
+    }
+
+    return obj;
+}
+
+void ConfigManager::handleAPGet() {
+    SPIFFS.begin();
+
+    File f = SPIFFS.open(apFilename, "r");
+    if (!f) {
+        Serial.println("file open failed");
+        server->send(500, "text/html", "File not found");
+        return;
+    }
+
+    String content = f.readString();
+    server->send(500, "text/html", content);
+
+    f.close();
+}
+
+void ConfigManager::reset() {
+  EEPROM.put(0, "BasicSSID");
+  EEPROM.put(32, "BasicPASSWORD");
+  EEPROM.commit();
+  delay(1000);
+  startAP();
+}
+
+void ConfigManager::handleAPPost() {
+    bool isJson = server->header("Content-Type") == "application/json";
+    String ssid;
+    String password;
+    String hospitalName;
+    String buildingName;
+    String floorNum;
+    String roomNum;
+    String bedNum;
+    char ssidChar[32];
+    char passwordChar[64];
+    char hospitalNameChar[64];
+    char buildingNameChar[32];
+    char floorNumChar[32];
+    char roomNumChar[32];
+    char bedNumChar[32];
+    bool resend = 0;
+    
+    for (int i = 0; i < server->args(); i++) 
+    {
+        if((server->arg(i) == "")&&(server->argName(i) != "password"))
+        {
+            //re-send data entry request
+            resend = 1;
+            break;
+        }
+        
+        if(i== 0)
+            {
+                ssid = server->arg(i);
+                strncpy(ssidChar, ssid.c_str(), sizeof(ssidChar));
+            }
+        else if(i==1)
+            {
+                password = server->arg(i);
+                strncpy(passwordChar, password.c_str(), sizeof(passwordChar));
+            }
+        else if(i==2)
+            {
+                hospitalName = server->arg(i);
+                strncpy(hospitalNameChar, hospitalName.c_str(), sizeof(hospitalNameChar));
+            }
+        else if(i==3)
+            {
+                buildingName = server->arg(i);
+                strncpy(buildingNameChar, buildingName.c_str(), sizeof(buildingNameChar));
+            }
+        else if(i==4)
+            {
+                floorNum = server->arg(i);
+                strncpy(floorNumChar, floorNum.c_str(), sizeof(floorNumChar));
+            }
+        else if(i==5)
+            {
+                roomNum = server->arg(i);
+                strncpy(roomNumChar, roomNum.c_str(), sizeof(roomNumChar));
+            }
+        else if(i==6)
+            {
+                bedNum = server->arg(i);
+                strncpy(bedNumChar, bedNum.c_str(), sizeof(bedNumChar));
+            }
+        else
+        {
+            // Add new params here
+        }
+    } 
+
+
+    if (resend)
+    {
+        // Allow entering no password. Rest of the fields are important to
+        // provision the device. So empty fields are not allowed
+        SPIFFS.begin();
+
+        File f = SPIFFS.open(apFilename2, "r");
+        if (!f) {
+            Serial.println("file open failed");
+            server->send(500, "text/html", "File not found");
+            return;
+        }
+
+        String content = f.readString();
+        server->send(500, "text/html", content);
+
+        f.close();
+        return;
+    }
+    else
+    {
+        Serial.println(ssidChar);
+        Serial.println(passwordChar);
+        Serial.println(hospitalNameChar);
+        Serial.println(buildingNameChar);
+        Serial.println(floorNumChar);
+        Serial.println(roomNumChar);
+        Serial.println(bedNumChar);
+        
+        EEPROM.put(0, ssidChar);
+        EEPROM.put(32, passwordChar);
+        EEPROM.put(96, hospitalNameChar);
+        EEPROM.put(160, buildingNameChar);
+        EEPROM.put(192, floorNumChar);
+        EEPROM.put(224, roomNumChar);
+        EEPROM.put(256, bedNumChar);
+        
+        EEPROM.commit();
+        server->send(201, "text/plain", "Saved. Will attempt to reboot.");
+        ESP.restart();
+    }
+}
+
+void ConfigManager::handleRESTGet() {
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& obj = jsonBuffer.createObject();
+
+    std::list<BaseParameter*>::iterator it;
+    for (it = parameters.begin(); it != parameters.end(); ++it) {
+        (*it)->toJson(&obj);
+    }
+
+    String body;
+    obj.printTo(body);
+
+    server->send(200, "application/json", body);
+}
+
+void ConfigManager::handleRESTPut() {
+    JsonObject& obj = this->decodeJson(server->arg("plain"));
+    if (!obj.success()) {
+        server->send(400, "application/json", "");
+        return;
+    }
+
+    std::list<BaseParameter*>::iterator it;
+    for (it = parameters.begin(); it != parameters.end(); ++it) {
+        (*it)->fromJson(&obj);
+    }
+
+    writeConfig();
+
+    server->send(204, "application/json", "");
+}
+
+void ConfigManager::handleNotFound() {
+    if (!isIp(server->hostHeader()) ) {
+        server->sendHeader("Location", String("http://") + toStringIP(server->client().localIP()), true);
+        server->send(302, "text/plain", ""); // Empty content inhibits Content-length header so we have to close the socket ourselves.
+        server->client().stop();
+        return;
+    }
+
+    server->send(404, "text/plain", "");
+    server->client().stop();
+}
+
+bool ConfigManager::wifiConnected() {
+    Serial.print("Waiting for WiFi to connect");
+    pinMode(PIN_RESET, INPUT);
+    /*int i = 0;
+    while (i < 20) {
+        if (WiFi.status() == WL_CONNECTED) {
+            Serial.println("");
+            return true;
+        }
+
+        Serial.print(".");
+
+        delay(500);
+        i++;
+    }
+
+    Serial.println("");
+    Serial.println("Connection timed out");
+
+    return false;*/
+
+    while(WiFi.status() != WL_CONNECTED){
+        reset();
+        Serial.println("Not connected to wifi, attemping to connect in 1 seconds again");
+        delay(1000);
+    }
+
+    if (WiFi.status() == WL_CONNECTED) {
+            Serial.println("");
+            Serial.println("connected");
+            return true;
+    }
+}
+
+void ConfigManager::setup() {
+    char ssid[32];
+    char password[64];
+
+    Serial.println("Reading saved configuration");
+
+    EEPROM.get(0, ssid);
+    EEPROM.get(32, password);
+    readConfig();
+
+    if (ssid != NULL) {
+        WiFi.begin(ssid, password[0] == '\0' ? NULL : password);
+        if (wifiConnected()) {
+            Serial.print("Connected to ");
+            Serial.print(ssid);
+            Serial.print(" with ");
+            Serial.println(WiFi.localIP());
+
+            WiFi.mode(WIFI_STA);
+            startApi();
+            return;
+        }
+    }
+
+    //startAP();
+}
+
+void ConfigManager::startAP() {
+    const char* headerKeys[] = {"Content-Type"};
+    size_t headerKeysSize = sizeof(headerKeys)/sizeof(char*);
+
+    //Serial.println("Starting Access Point");
+
+    IPAddress ip(192, 168, 1, 1);
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(ip, ip, IPAddress(255, 255, 255, 0));
+    WiFi.softAP(apName);
+
+    delay(500); // Need to wait to get IP
+
+    DNSServer dnsServer;
+    dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
+    dnsServer.start(DNS_PORT, "*", ip);
+
+    server.reset(new ESP8266WebServer(80));
+    server->collectHeaders(headerKeys, headerKeysSize);
+    server->on("/", HTTPMethod::HTTP_GET, std::bind(&ConfigManager::handleAPGet, this));
+    server->on("/", HTTPMethod::HTTP_POST, std::bind(&ConfigManager::handleAPPost, this));
+    server->onNotFound(std::bind(&ConfigManager::handleNotFound, this));
+    server->begin();
+
+    while (1) {
+        dnsServer.processNextRequest();
+        server->handleClient();
+    }
+}
+
+void ConfigManager::startApi() {
+    const char* headerKeys[] = {"Content-Type"};
+    size_t headerKeysSize = sizeof(headerKeys)/sizeof(char*);
+
+    server.reset(new ESP8266WebServer(80));
+    server->collectHeaders(headerKeys, headerKeysSize);
+    server->on("/", HTTPMethod::HTTP_GET, std::bind(&ConfigManager::handleAPGet, this));
+    server->on("/", HTTPMethod::HTTP_POST, std::bind(&ConfigManager::handleAPPost, this));
+    server->on("/settings", HTTPMethod::HTTP_GET, std::bind(&ConfigManager::handleRESTGet, this));
+    server->on("/settings", HTTPMethod::HTTP_PUT, std::bind(&ConfigManager::handleRESTPut, this));
+
+    if (apiCallback) {
+        apiCallback(server.get());
+    }
+
+    server->onNotFound(std::bind(&ConfigManager::handleNotFound, this));
+    server->begin();
+}
+
+void ConfigManager::readConfig() {
+    byte *ptr = (byte *)config;
+
+    for (int i = 0; i < configSize; i++) {
+        *(ptr++) = EEPROM.read(96 + i);
+    }
+}
+
+void ConfigManager::writeConfig() {
+    byte *ptr = (byte *)config;
+
+    for (int i = 0; i < configSize; i++) {
+        EEPROM.write(96 + i, *(ptr++));
+    }
+    EEPROM.commit();
+}
+
+boolean ConfigManager::isIp(String str) {
+  for (int i = 0; i < str.length(); i++) {
+    int c = str.charAt(i);
+    if (c != '.' && (c < '0' || c > '9')) {
+      return false;
+    }
+  }
+  return true;
+}
+
+String ConfigManager::toStringIP(IPAddress ip) {
+  String res = "";
+  for (int i = 0; i < 3; i++) {
+    res += String((ip >> (8 * i)) & 0xFF) + ".";
+  }
+  res += String(((ip >> 8 * 3)) & 0xFF);
+  return res;
+}
